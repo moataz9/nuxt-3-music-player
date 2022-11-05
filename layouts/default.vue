8fb8fb396990dfa1f8ddbc7cc42344f1f3d39ac7
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-11-06 01:00:51 +0200

    layouts: Default

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="relative flex">
+  <div class="relative flex min-h-screen">
     <Sidebar />
 
     <div class="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
       <Searchbar />
 
-      <div class="px-6 h-[clac(100vh-72px)] overflow-y-scroll overflow-hidden flex xl:flex-row flex-col-reverse" >
+      <div
+        class="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse"
+      >
         <div class="flex-1 h-fit pb-40">
           <slot />
         </div>
-        <div class="xl:sticky relative top-0 h-fit"></div>
+        <div class="xl:sticky relative top-0 h-fit">TopPlay</div>
       </div>
+    </div>
+
+    <div
+      class="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10"
+    >
+      <clientOnly>
+        <Music />
+      </clientOnly>
     </div>
   </div>
 </template>
