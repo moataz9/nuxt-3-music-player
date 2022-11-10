444424d29b9e271eeafa18a54d14d2b0f2cfd8f7
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-11-10 12:46:33 +0200

    index Page: remove songsLoder-store FNs

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-// import { storeToRefs } from 'pinia'
 import { storeToRefs } from 'pinia'
 import { genres } from '~~/constants'
 import { usePlayer } from '~~/store/player'
-import { useSongsLoader } from '~~/store/songsLoader'
+import { chartsWorld } from '~~/types'
+
+const { $APIs } = useNuxtApp()
 
 const genreListId = ref('POP')
 const genreTitle = ref('pop')
 
-const songsLoader = useSongsLoader()
-songsLoader.getTopCharts()
-const { topCharts } = storeToRefs(songsLoader)
+const { data: homeCharts } = useAsyncData(
+  'homeCharts',
+  () => $APIs.shazam.get('/charts/world') as Promise<chartsWorld[]>
+)
 
 const playerStore = usePlayer()
 const { togglePlaying } = playerStore
@@ -22,7 +24,6 @@ provide('isPlaying', isPlaying)
   <div class="flex flex-col">
     <div class="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
       <h2 class="font-bold text-3xl text-white text-left">Top Charts</h2>
-      <!-- @change="(e) => dispatch(selectGenreListId(e.target.value))" -->
       <select
         v-model="genreListId"
         class="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
@@ -35,11 +36,11 @@ provide('isPlaying', isPlaying)
 
     <div class="flex flex-wrap sm:justify-start justify-center gap-8">
       <SongCard
-        v-for="song in topCharts"
+        v-for="song in homeCharts"
         :key="song.key"
         :song="song"
         :active="song.key === currentSong?.key"
-        @togglePlaying="togglePlaying(song)"
+        @togglePlaying="togglePlaying(song, homeCharts as chartsWorld[])"
       />
     </div>
   </div>
--- store/songsLoader.ts
+++ store/songsLoader.ts
@@ -2,43 +2,43 @@ import { chartsWorld } from 'types'
 import { defineStore } from 'pinia'
 
 export const useSongsLoader = defineStore('songsLoader', () => {
-  const { $mande } = useNuxtApp()
+  const { $APIs } = useNuxtApp()
 
   const loadErr = ref(false)
   const topCharts = ref<chartsWorld[]>([])
 
-  async function getTopCharts()/* : Promise<chartsWorld[]> */ {
+  async function getTopCharts() /* : Promise<chartsWorld[]> */ {
     try {
-      const result = await $mande.get('/charts/world') as chartsWorld[];
+      const result = (await $APIs.shazam.get('/charts/world')) as chartsWorld[]
       topCharts.value = result
     } catch (err) {
-      console.log(err);
-      loadErr.value = true 
+      console.log(err)
+      loadErr.value = true
     }
   }
 
   async function getSongsByGenre(genre: string): Promise<chartsWorld[]> {
-    return await $mande.get(`/charts/genre-world?genre_code=${genre}`)
+    return await $APIs.shazam.get(`/charts/genre-world?genre_code=${genre}`)
   }
 
   async function getSongsByCountry(countryCode: string): Promise<chartsWorld[]> {
-    return await $mande.get(`/charts/country?country_code=${countryCode}`)
+    return await $APIs.shazam.get(`/charts/country?country_code=${countryCode}`)
   }
 
   async function getSongsBySearch(searchTerm: string): Promise<chartsWorld[]> {
-    return await $mande.get(`/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`)
+    return await $APIs.shazam.get(`/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`)
   }
 
   async function getArtistDetails(artistId: string) {
-    return await $mande.get(`/artists/details?artist_id=${artistId}`)
+    return await $APIs.shazam.get(`/artists/details?artist_id=${artistId}`)
   }
 
   async function getSongDetails(songid: string): Promise<chartsWorld[]> {
-    return await $mande.get(`/tracks/details?track_id=${songid}`)
+    return await $APIs.shazam.get(`/tracks/details?track_id=${songid}`)
   }
 
   async function getSongRelated(songid: string): Promise<chartsWorld[]> {
-    return await $mande.get(`/tracks/related?track_id=${songid}`)
+    return await $APIs.shazam.get(`/tracks/related?track_id=${songid}`)
   }
 
   return {
